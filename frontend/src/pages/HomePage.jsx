ad9634d107908e061cecd41a40bff518539e9c1b
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-08-23 11:36:16 +0700

    update explore

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,18 +13,24 @@ const HomePage = () => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // const [sortType, setSortType] = useState("forks");
+  const [sortType, setSortType] = useState("forks");
 
-  const getUserProfileAndRepos = useCallback(async() => {
+  const getUserProfileAndRepos = useCallback(async(username="anhoangcao") => {
     setLoading(true);
     try {
-      const userRes = await fetch('https://api.github.com/users/anhoangcao')
+      // 60 requests per hour, 5000 requests per hour for authenticated requests
+      const userRes = await fetch(`https://api.github.com/users/${username}`, {
+        headers: {
+          authorization: `token`,
+        }
+      });
       const userProfile =await userRes.json();
       setUserProfile(userProfile);
 
       const repoRes = await fetch(userProfile.repos_url);
       const repos = await repoRes.json();
       setRepos(repos);
+      return {userProfile, repos}
       
     } catch (error) {
       toast.error(error.message);
@@ -37,14 +43,40 @@ const HomePage = () => {
     getUserProfileAndRepos();
   },[getUserProfileAndRepos]);
 
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+    
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const {userProfile, repos} = await getUserProfileAndRepos(username);
+
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+  }
+
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); // descending, recent first
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count); // descending, most starts first
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count); // descending, most forks first
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  }
+
   return (
     <div className="m-4">
-      <Search />
-      <SortRepos />
+      <Search onSearch={onSearch} />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
         
-        {repos.length > 0 && !loading && <Repos repos={repos} />}
+        {!loading && <Repos repos={repos} />}
         {loading && <Spinner />}
       </div>
     </div>
