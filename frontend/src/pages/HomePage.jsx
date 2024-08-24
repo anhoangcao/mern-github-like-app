b69fe88ad9eccce3824c62808dcc32317cb449a8
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-08-24 14:42:46 +0700

    api routes setup

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -17,21 +17,15 @@ const HomePage = () => {
   const getUserProfileAndRepos = useCallback(async(username="anhoangcao") => {
     setLoading(true);
     try {
-      // 60 requests per hour, 5000 requests per hour for authenticated requests
-      const userRes = await fetch(`https://api.github.com/users/${username}`, {
-        headers: {
-          authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-        }
-      });
-      const userProfile =await userRes.json();
+      const res = await fetch(`http://localhost:5000/api/users/profile/${username}`);
+      const { repos, userProfile } = await res.json();
+
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); // descending, recent first
+      
+      setRepos(repos);
       setUserProfile(userProfile);
 
-      const repoRes = await fetch(userProfile.repos_url);
-      const repos = await repoRes.json();
-      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); // descending, recent first
-      setRepos(repos);
-      return {userProfile, repos}
-      
+      return {userProfile, repos}      
     } catch (error) {
       toast.error(error.message);
     } finally {
